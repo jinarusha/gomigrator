0f41bf82253a5ac897dae42cee09087312d3e041
Jin Hun Park <jinhunpark@malangstudio.com>
2016-08-11 17:16:31 +0900

    Add channel to handle jobs. Minor cleanup

--- migrator.go
+++ migrator.go
@@ -3,6 +3,7 @@ package gomigrator
 import (
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/juju/errors"
 	"github.com/siddontang/go-mysql/client"
@@ -35,15 +36,21 @@ type Migrator struct {
 
 	masterConn *client.Conn
 	slaveConn  *client.Conn
+
+	eventQueue   chan *replication.BinlogEvent
+	MaxQueueSize int
 }
 
-func (m *Migrator) initSlaveDBConn() {
+func (m *Migrator) init() {
 	conn, err := client.Connect(m.SlaveHost+":"+strconv.Itoa(int(m.SlavePort)), m.SlaveUser, m.SlavePassword, m.SlaveDatabaseName)
 	if err != nil {
 		panic(err)
 	}
 	m.slaveConn = conn
 	log.Infof("Connection to target DB made")
+
+	m.eventQueue = make(chan *replication.BinlogEvent, m.MaxQueueSize)
+
 }
 
 func (m *Migrator) close() {
@@ -55,15 +62,18 @@ func (m *Migrator) close() {
 		log.Infof("Closing connection to target DB")
 		m.slaveConn.Close()
 	}
+
 }
 
-func (m *Migrator) StartSync(serverId uint32, serverType string) {
+// Starts replication
+func (m *Migrator) StartSync(serverId uint32, serverType string) error {
 	defer m.close()
 
-	m.initSlaveDBConn()
+	m.init()
 
 	syncer := replication.NewBinlogSyncer(serverId, serverType)
 	syncer.RegisterSlave(m.MasterHost, m.MasterPort, m.MasterUser, m.MasterPassword)
+	defer syncer.Close()
 
 	pos := mysql.Position{m.BinlogFilename, m.BinlogPosition}
 	streamer, err := syncer.StartSync(pos)
@@ -71,23 +81,35 @@ func (m *Migrator) StartSync(serverId uint32, serverType string) {
 		panic(err)
 	}
 
+	go m.handleEvent()
+
+	timeout := 10 * time.Second
 	for {
-		ev, err := streamer.GetEvent()
-		if err != nil {
-			panic(err)
+		event, err := streamer.GetEventTimeout(timeout)
+		if err != nil && err != replication.ErrGetEventTimeout {
+			log.Errorf("syncing error: %v", err)
+			return err
+		} else if err == replication.ErrGetEventTimeout {
+			log.Infof("errGetEventTimeout: waiting for further event. Retrying in %d. Error: %v", timeout, err)
+			continue
 		}
 
-		switch e := ev.Event.(type) {
+		switch e := event.Event.(type) {
 		case *replication.RotateEvent:
-			pos.Name = string(e.NextLogName)
-			pos.Pos = uint32(e.Position)
-			log.Infof("Rotating binlog to %v", pos)
+			m.BinlogFilename = string(e.NextLogName)
+			m.BinlogPosition = uint32(e.Position)
+			log.Infof("Rotating binlog to %v", mysql.Position{string(e.NextLogName), uint32(e.Position)})
 		default:
-			// handleRowsEvent returns error when event is none of insert/update/delete
-			// So you can ignore errors here if all you care about is insert/update/delete
-			if err := m.handleRowsEvent(ev); err != nil {
-				continue
-			}
+			m.eventQueue <- event
+		}
+	}
+}
+
+func (m *Migrator) handleEvent() {
+	for {
+		select {
+		case event := <-m.eventQueue:
+			m.handleRowsEvent(event)
 		}
 	}
 }
@@ -98,7 +120,7 @@ func (m *Migrator) handleRowsEvent(e *replication.BinlogEvent) error {
 	case replication.WRITE_ROWS_EVENTv1, replication.WRITE_ROWS_EVENTv2:
 		ev := e.Event.(*replication.RowsEvent)
 		table := string(ev.Table.Table)
-		m.upsertRow(table, ev.Rows[0], "insert")
+		m.upsertRow(table, ev.Rows[0])
 	case replication.DELETE_ROWS_EVENTv1, replication.DELETE_ROWS_EVENTv2:
 		ev := e.Event.(*replication.RowsEvent)
 		table := string(ev.Table.Table)
@@ -113,7 +135,7 @@ func (m *Migrator) handleRowsEvent(e *replication.BinlogEvent) error {
 		if beforePk != afterPk {
 			m.deleteRow(table, ev.Rows[0])
 		}
-		m.upsertRow(table, ev.Rows[1], "update")
+		m.upsertRow(table, ev.Rows[1])
 	default:
 		return errors.Errorf("%s not supported now", e.Header.EventType)
 	}
@@ -159,13 +181,11 @@ func (m *Migrator) deleteRow(table string, params []interface{}) (*mysql.Result,
 	result, err := m.execute(query, paramsWithoutNil)
 	if err != nil {
 		log.Error("deleteRow: err: ", err)
-	} else {
-		log.Info("Deleting from ", table)
 	}
 	return result, err
 }
 
-func (m *Migrator) upsertRow(table string, params []interface{}, action string) (*mysql.Result, error) {
+func (m *Migrator) upsertRow(table string, params []interface{}) (*mysql.Result, error) {
 	colSize := len(params)
 	paramStr := ""
 	for idx := 0; idx < colSize; idx++ {
@@ -178,13 +198,7 @@ func (m *Migrator) upsertRow(table string, params []interface{}, action string) 
 	query := fmt.Sprintf(replaceSql, table, paramStr)
 	result, err := m.execute(query, params)
 	if err != nil {
-		log.Error("deleteRow: err: ", err)
-	} else {
-		if action == "insert" {
-			log.Info("Inserting into ", table)
-		} else if action == "update" {
-			log.Info("Updating in ", table)
-		}
+		log.Error("upsertRow: err: ", err)
 	}
 	return result, err
 }
